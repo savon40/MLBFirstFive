3a9c6373f6fd1b6c7ba07d25ed28eb5936d26c84
Steve Avon <steveavon@Steves-MacBook-Pro.local>
2021-05-01 21:42:35 -0400

    moved stuff to data folder

--- script.py
+++ script.py
@@ -39,41 +39,44 @@ def getTodaysGames(today):
     matchup_divs = soup.find_all("div", {"class": "starting-lineups__matchup"})
     final_matchups = []
 
+    i = 1
     for matchup_div in matchup_divs:
 
-        away_div = matchup_div.find(
-            "span", {"class": "starting-lineups__team-name--away"})
-        home_div = matchup_div.find(
-            "span", {"class": "starting-lineups__team-name--home"})
-        home_a_tag = home_div.find('a', href=True)
-        away_a_tag = away_div.find('a', href=True)
+        if i == 10 or i == 11:
+            away_div = matchup_div.find(
+                "span", {"class": "starting-lineups__team-name--away"})
+            home_div = matchup_div.find(
+                "span", {"class": "starting-lineups__team-name--home"})
+            home_a_tag = home_div.find('a', href=True)
+            away_a_tag = away_div.find('a', href=True)
 
-        # time
-        time_div = matchup_div.find(
-            "div", {"class": "starting-lineups__game-date-time"})
-        time = time_div.find("time")
+            # time
+            time_div = matchup_div.find(
+                "div", {"class": "starting-lineups__game-date-time"})
+            time = time_div.find("time")
 
-        home = {
-            "team": home_a_tag.contents[0].strip(),
-        }
-        away = {
-            "team": away_a_tag.contents[0].strip(),
-        }
+            home = {
+                "team": home_a_tag.contents[0].strip(),
+            }
+            away = {
+                "team": away_a_tag.contents[0].strip(),
+            }
 
-        pitcher_divs = matchup_div.find_all(
-            "div", {"class": "starting-lineups__pitchers"})
-        pitchers = getPitcher(pitcher_divs)
-        lineups = getLineups(matchup_div)
+            pitcher_divs = matchup_div.find_all(
+                "div", {"class": "starting-lineups__pitchers"})
+            pitchers = getPitcher(pitcher_divs)
+            lineups = getLineups(matchup_div)
 
-        matchup = {
-            "datetime": time.get('datetime'),
-            "home": home,
-            "away": away,
-            "pitchers": pitchers,
-            "lineups": lineups
-        }
-        final_matchups.append(matchup)
+            matchup = {
+                "datetime": time.get('datetime'),
+                "home": home,
+                "away": away,
+                "pitchers": pitchers,
+                "lineups": lineups
+            }
+            final_matchups.append(matchup)
         # break
+        i = i + 1
         # exit()
 
     return final_matchups
@@ -85,11 +88,11 @@ def main():
 
     # gathering
     matchups = getTodaysGames(today)
-    with open(f"{str(today)}-raw.json", 'w') as fp:
+    with open(f"data/{str(today)}-raw.json", 'w') as fp:
         json.dump(matchups, fp)
 
     # reading
-    f = open(f"{str(today)}-raw.json",)  # Opening JSON file
+    f = open(f"data/{str(today)}-raw.json",)  # Opening JSON file
     # f = open(f"2021-04-19.json",)  # Opening JSON file
     data = json.load(f)  # returns JSON object as a dictionary
     final = findWinners(data)
@@ -98,12 +101,12 @@ def main():
     # print(final)
 
     # with open(f"2021-04-19-final.json", 'w') as fp:
-    with open(f"{str(today)}-final.json", 'w') as fp:
+    with open(f"data/{str(today)}-final.json", 'w') as fp:
         json.dump(final, fp)
 
 
     #email results
-    with open(f"{str(today)}-final.json", 'rb') as content_file:
+    with open(f"data/{str(today)}-final.json", 'rb') as content_file:
 
         sender_address = "seavon.sf@gmail.com"
         sender_password = "Duecourse_1"
