ebc8904f2ede450167d54162413123c430f4bf0c
Steve Avon <steveavon@Steves-MacBook-Pro.local>
2021-05-04 13:32:44 -0400

    fixes, planning fangraphs, adding jack

--- script.py
+++ script.py
@@ -40,44 +40,45 @@ def getTodaysGames(today):
     matchup_divs = soup.find_all("div", {"class": "starting-lineups__matchup"})
     final_matchups = []
 
-    i = 1
+    # i = 1
     for matchup_div in matchup_divs:
 
-        if i == 10 or i == 11:
-            away_div = matchup_div.find(
-                "span", {"class": "starting-lineups__team-name--away"})
-            home_div = matchup_div.find(
-                "span", {"class": "starting-lineups__team-name--home"})
-            home_a_tag = home_div.find('a', href=True)
-            away_a_tag = away_div.find('a', href=True)
+        # if i == 10 or i == 11:
+        away_div = matchup_div.find(
+            "span", {"class": "starting-lineups__team-name--away"})
+        home_div = matchup_div.find(
+            "span", {"class": "starting-lineups__team-name--home"})
+        home_a_tag = home_div.find('a', href=True)
+        away_a_tag = away_div.find('a', href=True)
 
-            # time
-            time_div = matchup_div.find(
-                "div", {"class": "starting-lineups__game-date-time"})
-            time = time_div.find("time")
+        # time
+        time_div = matchup_div.find(
+            "div", {"class": "starting-lineups__game-date-time"})
+        time = time_div.find("time")
 
-            home = {
-                "team": home_a_tag.contents[0].strip(),
-            }
-            away = {
-                "team": away_a_tag.contents[0].strip(),
-            }
+        home = {
+            "team": home_a_tag.contents[0].strip(),
+        }
+        away = {
+            "team": away_a_tag.contents[0].strip(),
+        }
 
-            pitcher_divs = matchup_div.find_all(
-                "div", {"class": "starting-lineups__pitchers"})
-            pitchers = getPitcher(pitcher_divs)
-            lineups = getLineups(matchup_div)
+        pitcher_divs = matchup_div.find_all(
+            "div", {"class": "starting-lineups__pitchers"})
+        pitchers = getPitcher(pitcher_divs)
+        lineups = getLineups(matchup_div)
 
-            matchup = {
-                "datetime": time.get('datetime'),
-                "home": home,
-                "away": away,
-                "pitchers": pitchers,
-                "lineups": lineups
-            }
-            final_matchups.append(matchup)
+        matchup = {
+            "datetime": time.get('datetime'),
+            "home": home,
+            "away": away,
+            "pitchers": pitchers,
+            "lineups": lineups
+        }
+        final_matchups.append(matchup)
+
         # break
-        i = i + 1
+        # i = i + 1
         # exit()
 
     return final_matchups
@@ -114,7 +115,7 @@ def main():
         sender_address = "seavon.sf@gmail.com"
         sender_password = "Duecourse_1"
 
-        receiver_address = ["savon40@gmail.com", "savon@bhg-inc.com"]
+        receiver_address = ["savon40@gmail.com", "jackcanaley@gmail.com"]
 
         msg = EmailMessage()
 
